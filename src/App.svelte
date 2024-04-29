806d7610fb6e025b0f627efbedb9d13b6645d20d
ElecarnoYT <benrobmc.mc@gmail.com>
2024-04-29 21:12:52 +0100

    question viewer v1, indentification of question 1 pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,33 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+import output_txt from "../output/parsed.txt?raw"
+import output_json from "../output/parsed.json?raw"
+
+// let parsed_json = JSON.parse(output_json)
+// console.log(parsed_json)
+
+import PdfViewer from 'svelte-pdf';
+import PdfQuestionViewer from "./lib/PdfQuestionViewer.svelte"
+import PdfMIViewer from "./lib/PdfMIViewer.svelte"
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="question-viewer">
+    <PdfQuestionViewer url="../sqa_pdfs/NH_Graphic-Communication_QP_2023.pdf" />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div class="mi-viewer">
+    <PdfMIViewer url="../sqa_pdfs/mi_NH_Graphic-Communication_mi_2023.pdf" />
   </div>
+  
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .question-viewer {
+    width: 52%;
+    float: left;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .mi-viewer {
+    width: 47%;
+    float: right;
   }
 </style>
