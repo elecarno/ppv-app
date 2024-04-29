9a9980d0768c25b81d833dea5b232442c2cc9e13
ElecarnoYT <benrobmc.mc@gmail.com>
2024-04-29 18:26:48 +0100

    parser functionality

--- parser.js
+++ parser.js
@@ -5,7 +5,7 @@ const pdfParser = new PDFParser(this,1);
 
 const output_path_txt = "/0_projects/programming_projects/sqa_viewer/output/parsed.txt"
 const output_path_json = "/0_projects/programming_projects/sqa_viewer/output/parsed.json"
-const load_path = "/0_projects/programming_projects/sqa_viewer/sqa_pdfs/nq-2024-exam-timetable.pdf"
+const load_path = "/0_projects/programming_projects/sqa_viewer/sqa_pdfs/NH_Spanish_Directed-Writing_2023.pdf"
 
 pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
 pdfParser.on("pdfParser_dataReady", pdfData => {
@@ -14,7 +14,6 @@ pdfParser.on("pdfParser_dataReady", pdfData => {
 });
 
 pdfParser.loadPDF(load_path);
-
 
 // "/0_projects/programming_projects/sqa_viewer/output/parsed.json"
 // "/0_projects/programming_projects/sqa_viewer/sqa_pdfs/orwell_first_draft.pdf"
